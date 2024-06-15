9f1b2fe74acbfecce74f53dce4a1182e8d59fe95
Prathmesh-Kamble-10 <prathmeshkamble885@gmail.com>
2024-06-16 00:28:51 +0530

    Git new Push

---
+++ projects/script.js
@@ -0,0 +1,14 @@
+var bulb = document.querySelector('#box');
+var btn = document.querySelector('button');
+var flag =0;
+btn.addEventListener("click", function(){
+    if(flag==0){
+    bulb.style.backgroundColor ="Yellow";
+    btn.innerHTML = "ON"
+    flag =1
+    }else{
+        bulb.style.backgroundColor = "transparent";
+        btn.innerHTML = "OFF"
+        flag=0;
+    }
+})
